89ba6fd3aa69990bda4fc73f80f449726c59937d
Võ Trí Hiệu <142471760+trihieuvo@users.noreply.github.com>
2025-10-16 13:10:28 +0700

    Delete main directory
    tối ưu

--- main/java/com/baohanh/trungtambaohanh/repository/LoaiThietBiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baohanh.trungtambaohanh.repository;
-
-import com.baohanh.trungtambaohanh.entity.LoaiThietBi;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoaiThietBiRepository extends JpaRepository<LoaiThietBi, Integer> {
-}
--- main/java/com/baohanh/trungtambaohanh/service/DashboardService.java
+++
@@ -1,7 +0,0 @@
-package com.baohanh.trungtambaohanh.service;
-
-import com.baohanh.trungtambaohanh.dto.DashboardStatsDto;
-
-public interface DashboardService {
-    DashboardStatsDto getDashboardStats();
-}
